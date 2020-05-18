0d53a13f62120823439c3e87a0c12b9c51fcb7a2
Moin <kmoin786@gmail.com>
2020-05-19 00:35:27 +0530

    create controller and view for article

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_05_18_161213) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
